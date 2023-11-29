2da1ba750a703b7603c9e6619fc650756c540b17
Birwa Balar <balarbirwa@gmail.com>
2023-11-29 11:03:31 -0700

    person

--- person/Person.java
+++ person/Person.java
@@ -1,3 +1,32 @@
-public class Person {
-    
+public class Person extends Profile {
+    private Interests interests;
+
+    // Default constructor
+    public Person() {
+        super(); // Calls the default constructor of Profile
+        this.interests = new Interests("", "", "", "", "", "", "", "", "", "", "");
+    }
+
+    // Parameterized constructor
+    public Person(int age, int height, String gender, String sign, String job, Interests interests) {
+        super(age, height, gender, sign, job); // Calls the parameterized constructor of Profile
+        this.interests = interests;
+    }
+
+    // Getter and setter for interests
+    public Interests getInterests() {
+        return interests;
+    }
+
+    public void setInterests(Interests interests) {
+        this.interests = interests;
+    }
+
+    @Override
+    public String toString() {
+        return "Person {" +
+               "\n  Profile: " + super.toString() + // Calls toString of Profile class
+               ",\n  Interests: " + interests.toString() + // Calls toString of Interests class
+               "\n}";
+    }
 }
